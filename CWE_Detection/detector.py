b50ea3ec2e59ae27207a25dcf668dee7dd91de58
John Parks <johnwparks03@gmail.com>
2025-08-15 14:33:04 -0400

    Add CWE 1233 detection Save results in .xlsx file now Change data frame column names to enums

--- CWE_Detection/detector.py
+++ CWE_Detection/detector.py
@@ -7,6 +7,8 @@ import pandas as pd
 import tkinter as tk
 from tkinter import filedialog
 import time
+from collections import defaultdict
+from enums import *
 
 
 def load_json_file(file_path: str) -> dict:
@@ -90,34 +92,39 @@ def select_files_for_detection() -> str | None:
     root.mainloop()
 
     # If a selection was made return the selected path
-    if root.winfo_exists():
+    try:
         root.destroy()
         return selected['path']
-    else:
+    except:
         print("No selection made. Exiting.")
         return None        
 
-def save_results(results_df: pd.DataFrame):
+def save_results(CWE_1245_results_df: pd.DataFrame, CWE_1233_results_df: pd.DataFrame, detection_statistics_df: pd.DataFrame):
     """Opens dialog to save results
 
     Args:
-        results_df (pd.DataFrame): Results to save
+        CWE_1245_results_df (pd.DataFrame): CWE 1245 detection results
+        CWE_1233_results_df (pd.DataFrame): CWE 1233 detection results
     """
     root = tk.Tk()
     root.withdraw()  # Hide the root window
 
     # Open dialog box to save results file as csv
     save_file_path = filedialog.asksaveasfilename(
-        defaultextension=".csv",  # Default file extension
-        filetypes=[("CSV files", "*.csv"), ("All files", "*.*")],  # File type filters
-        title=f"All cases processed. Where would you like to save the results?",  # Dialog box title
+        defaultextension=".xlsx",  # Default file extension
+        filetypes=[("Excel files", "*.xlsx"), ("All files", "*.*")],  # File type filters
+        title=f"All files processes. Where would you like to save the results?",  # Dialog box title
         initialdir="/media/sf_Summer_Research/CWE_Detection/Results",  # Initial directory
     )
 
     #Try to save file
     if save_file_path:
         try:
-            results_df.to_csv(save_file_path, index=False)
+            # results_df.to_csv(save_file_path, index=False)
+            with pd.ExcelWriter(save_file_path) as writer:
+                CWE_1245_results_df.to_excel(writer, sheet_name="CWE 1245 Results", index=False)
+                CWE_1233_results_df.to_excel(writer, sheet_name="CWE 1233 Results", index=False)
+                detection_statistics_df.to_excel(writer, sheet_name="Detection Statistics", index=False)
             print(f"Results saved successfully to {save_file_path}")
         except Exception as e:
             print(f"Error saving file: {e}")
@@ -322,9 +329,43 @@ def detect_CWE_1245(ast_data: AST_Traverser, case_statement: HdlStmCaseNode, res
         else:
             return f"Secure: No deadlocks detected"
 
-    results['state_coverage'] = (check_state_coverage(case_statement))
-    results['unreachable_states'] = (check_unreachable_states(case_statement))
-    results['deadlocks'] = (check_for_deadlocks(case_statement))
+    results[CWE_1245_RESULTS_DF_COLS.STATE_COVERAGE.value] = (check_state_coverage(case_statement))
+    results[CWE_1245_RESULTS_DF_COLS.UNREACHABLE_STATES.value] = (check_unreachable_states(case_statement))
+    results[CWE_1245_RESULTS_DF_COLS.DEADLOCKS.value] = (check_for_deadlocks(case_statement))
+
+    return results
+
+def detect_CWE_1233(ast_data: AST_Traverser, results: pd.DataFrame) -> tuple[pd.DataFrame, pd.DataFrame, pd.DataFrame]:
+    #Scenario 1: All security sensitive register assignments are locked
+    def verify_security_sensitive_register_coverage():
+        unprotected_register_assignments = defaultdict(list) #{register name: assignment}
+
+        for security_sensitive_register in ast_data.security_sensitive_registers:
+            assignments = ast_data.variable_assignments[security_sensitive_register.name]
+            #Check that each assignment is lock bit protected
+            for assignment in assignments:
+                if assignment.zeroized or assignment.source == assignment.destination:
+                    #If the register is zeroized or set to itself it does not need to be protected by a lock bit
+                    continue
+                elif not assignment.lock_bit_protected:
+                    #Assignment is not protected through lock bit
+                    unprotected_register_assignments[security_sensitive_register.name].append(assignment)
+        
+        if len(unprotected_register_assignments) > 0:
+            #TO-DO: Add in something to show which assignments are not protected, maybe line #
+            return f"Vulnerable: {[reg_name for reg_name in unprotected_register_assignments.keys()]} assignment(s) not protected"
+        else:
+            return "Secure: All security sensitive register assignments are protected"
+
+
+
+    #Scenario 2: All locked assignments reject unauthorized writes
+    def verify_lock_enforcement_completeness():
+        pass
+
+    verify_lock_enforcement_completeness()
+    results[CWE_1233_RESULTS_DF_COLS.SECURITY_SENSITIVE_REGISTERS.value] = [node.name for node in ast_data.security_sensitive_registers]
+    results[CWE_1233_RESULTS_DF_COLS.SECURITY_SENSITIVE_REGISTER_COVERAGE.value] = verify_security_sensitive_register_coverage()
 
     return results
 
@@ -335,12 +376,16 @@ def run_detection_on_file(file_path: str) -> pd.DataFrame:
         file_path (str): File path of file for detection
 
     Returns:
-        pd.DataFrame: Results of detection on the file
+        tuple (pd.Dataframe, pd.Dataframe, pd.Dataframe): First dataframe contains CWE 1245 detection results,
+                                                          second dataframe contains CWE 1233 detection results,
+                                                          third dataframe contains detection statistics
     """
     start_time = time.perf_counter()
+    file_name = os.path.basename(file_path)
 
-    results_df_columns = ['file_name', 'case_number', 'state_coverage', 'unreachable_states', 'deadlocks', 'LoC', 'detection_time']
-    results_df = pd.DataFrame(columns=results_df_columns)
+    CWE_1245_results_df = pd.DataFrame(columns=[col.value for col in CWE_1245_RESULTS_DF_COLS])
+    CWE_1233_results_df = pd.DataFrame(columns=[col.value for col in CWE_1233_RESULTS_DF_COLS])
+    detection_statistics_df = pd.DataFrame(columns=[col.value for col in DETECTION_STATISTICS_DF_COLS])
 
     #Load JSON and traverse
     json_data = load_json_file(file_path)
@@ -356,101 +401,118 @@ def run_detection_on_file(file_path: str) -> pd.DataFrame:
             traverser.unsatisfiable_conditionals.pop(cond.node_id)
     del unsatisfiable_conditionals
 
+    #CWE 1245 Detection
     if len(traverser.case_statements) == 0:
         #If there are no case statements return the one row 
-        end_time = time.perf_counter()
-
-        results_df = pd.concat([results_df, pd.DataFrame([{
-            'file_name': os.path.basename(file_path),
-            'case_number': 'No case statements found',
-            'state_coverage': None,
-            'unreachable_states': None,
-            'deadlocks': None,
-            'LoC': traverser.loc,
-            'detection_time': (end_time - start_time)
+        CWE_1245_results_df = pd.concat([CWE_1245_results_df, pd.DataFrame([{
+            CWE_1245_RESULTS_DF_COLS.FILE_NAME.value: file_name,
+            CWE_1245_RESULTS_DF_COLS.CASE_NUMBER.value: 'No case statements found',
+            CWE_1245_RESULTS_DF_COLS.STATE_COVERAGE.value: None,
+            CWE_1245_RESULTS_DF_COLS.UNREACHABLE_STATES.value: None,
+            CWE_1245_RESULTS_DF_COLS.DEADLOCKS.value: None,
         }])], ignore_index=True)
-        return results_df
     else:
         case_num = 1
         #Loop through each case statement and run detection
         for case in traverser.case_statements.values():
             results_row = {
-                'file_name': os.path.basename(file_path),
-                'case_number': case_num,
-                'state_coverage': None,
-                'unreachable_states': None,
-                'deadlocks': None,
-                'LoC': traverser.loc
+                CWE_1245_RESULTS_DF_COLS.FILE_NAME.value: file_name,
+                CWE_1245_RESULTS_DF_COLS.CASE_NUMBER.value: case_num,
+                CWE_1245_RESULTS_DF_COLS.STATE_COVERAGE.value: None,
+                CWE_1245_RESULTS_DF_COLS.UNREACHABLE_STATES.value: None,
+                CWE_1245_RESULTS_DF_COLS.DEADLOCKS.value: None,
             }
             detect_CWE_1245(traverser, case, results=results_row)
 
-            end_time = time.perf_counter()
-            results_row['detection_time'] = (end_time - start_time)
-
             #Concat current results and new results row
-            results_df = pd.concat([results_df, pd.DataFrame([results_row])], ignore_index=True)
+            CWE_1245_results_df = pd.concat([CWE_1245_results_df, pd.DataFrame([results_row])], ignore_index=True)
             case_num += 1
     
-    return results_df
+    #CWE 1233 Detection
+    CWE_1233_results_row = {
+        CWE_1233_RESULTS_DF_COLS.FILE_NAME.value: file_name,
+        CWE_1233_RESULTS_DF_COLS.SECURITY_SENSITIVE_REGISTERS.value: None,
+        CWE_1233_RESULTS_DF_COLS.LOCK_ENFORCEMENT.value: None,
+        CWE_1233_RESULTS_DF_COLS.SECURITY_SENSITIVE_REGISTER_COVERAGE.value: None,
+    }
+    detect_CWE_1233(ast_data=traverser, results=CWE_1233_results_row)
+    CWE_1233_results_df = pd.concat([CWE_1233_results_df, pd.DataFrame([CWE_1233_results_row])], ignore_index=True)
 
-def run_detection_on_folder(folder_path: str) -> pd.DataFrame:
+    #Calculate end of detection time here and assign to rows in each dataframe
+    end_time = time.perf_counter()
+    detection_time = end_time - start_time
+    detection_statistics_df = pd.concat([
+        detection_statistics_df,
+        pd.DataFrame([{
+            DETECTION_STATISTICS_DF_COLS.FILE_NAME.value: file_name,
+            DETECTION_STATISTICS_DF_COLS.LINES_OF_CODE.value: traverser.loc,
+            DETECTION_STATISTICS_DF_COLS.DETECTION_TIME.value: detection_time
+        }])
+    ], ignore_index=True)
+
+    return CWE_1245_results_df, CWE_1233_results_df, detection_statistics_df
+
+def run_detection_on_folder(folder_path: str) -> tuple[pd.DataFrame, pd.DataFrame, pd.DataFrame]:
     """Runs vulnerability detection on each file in the folder at the specified path
 
     Args:
         folder_path (str): Folder to run detection on
 
     Returns:
-        pd.DataFrame: Dataframe containing the results of detection
+        tuple (pd.Dataframe, pd.Dataframe, pd.Dataframe): First dataframe contains CWE 1245 detection results,
+                                                          second dataframe contains CWE 1233 detection results,
+                                                          third dataframe contains detection statistics
     """
-    results_df_columns = ['file_name', 'case_number', 'state_coverage', 'unreachable_states', 'deadlocks']
-    results_df = pd.DataFrame(columns=results_df_columns)
+    CWE_1245_results_df = pd.DataFrame(columns=[col.value for col in CWE_1245_RESULTS_DF_COLS])
+    CWE_1233_results_df = pd.DataFrame(columns=[col.value for col in CWE_1233_RESULTS_DF_COLS])
+    detection_statistics_df = pd.DataFrame(columns=[col.value for col in DETECTION_STATISTICS_DF_COLS])
 
     #Loop through each file in the folder
     for file in os.listdir(folder_path):
         file_path = os.path.join(folder_path, file)
-
-        results_df = pd.concat([results_df, run_detection_on_file(file_path)], ignore_index=True)
+        new_CWE_1245_results, new_CWE_1233_results, new_detection_stats = run_detection_on_file(file_path)
+        CWE_1245_results_df = pd.concat([CWE_1245_results_df, new_CWE_1245_results], ignore_index=True)
+        CWE_1233_results_df = pd.concat([CWE_1233_results_df, new_CWE_1233_results], ignore_index=True)
+        detection_statistics_df = pd.concat([detection_statistics_df, new_detection_stats], ignore_index=True)
         print(f"\nProcessed file: {file}\n")
 
-    return results_df
+    return CWE_1245_results_df, CWE_1233_results_df, detection_statistics_df
 
 def main():
     selected_path = select_files_for_detection()
 
     #Run folder or file detection based on selected path
     if os.path.isdir(selected_path):
-        results_df = run_detection_on_folder(selected_path)
+        CWE_1245_results_df, CWE_1233_results_df, detection_statistics_df = run_detection_on_folder(selected_path)
     elif os.path.isfile(selected_path):
-        results_df = run_detection_on_file(selected_path)
+        CWE_1245_results_df, CWE_1233_results_df, detection_statistics_df = run_detection_on_file(selected_path)
     else:
         print("Invalid selection. Please select a valid file or folder.")
         return
     
-    #Create a row to contain the totals
-    total_row = pd.DataFrame([{
-        'file_name': 'Total',
-        'case_number': results_df['case_number'].count(),
-        'state_coverage': {
-            'Secure': results_df['state_coverage'].str.startswith('Secure').sum(),
-            'Vulnerable': results_df['state_coverage'].str.startswith('Vulnerable').sum(),
-            'Inconclusive': results_df['state_coverage'].str.startswith('Inconclusive').sum()
+    #Create a row to contain the totals for CWE 1245 results
+    CWE_1245_total_row = pd.DataFrame([{
+        CWE_1245_RESULTS_DF_COLS.FILE_NAME.value: 'Total',
+        CWE_1245_RESULTS_DF_COLS.CASE_NUMBER.value: CWE_1245_results_df[CWE_1245_RESULTS_DF_COLS.CASE_NUMBER.value].count(),
+        CWE_1245_RESULTS_DF_COLS.STATE_COVERAGE.value: {
+            'Secure': CWE_1245_results_df[CWE_1245_RESULTS_DF_COLS.STATE_COVERAGE.value].str.startswith('Secure').sum(),
+            'Vulnerable': CWE_1245_results_df[CWE_1245_RESULTS_DF_COLS.STATE_COVERAGE.value].str.startswith('Vulnerable').sum(),
+            'Inconclusive': CWE_1245_results_df[CWE_1245_RESULTS_DF_COLS.STATE_COVERAGE.value].str.startswith('Inconclusive').sum()
         },
-        'unreachable_states': {
-            'Secure': results_df['unreachable_states'].str.startswith('Secure').sum(),
-            'Vulnerable': results_df['unreachable_states'].str.startswith('Vulnerable').sum(),
-            'Inconclusive': results_df['unreachable_states'].str.startswith('Inconclusive').sum()
+        CWE_1245_RESULTS_DF_COLS.UNREACHABLE_STATES.value: {
+            'Secure': CWE_1245_results_df[CWE_1245_RESULTS_DF_COLS.UNREACHABLE_STATES.value].str.startswith('Secure').sum(),
+            'Vulnerable': CWE_1245_results_df[CWE_1245_RESULTS_DF_COLS.UNREACHABLE_STATES.value].str.startswith('Vulnerable').sum(),
+            'Inconclusive': CWE_1245_results_df[CWE_1245_RESULTS_DF_COLS.UNREACHABLE_STATES.value].str.startswith('Inconclusive').sum()
         },
-        'deadlocks': {
-            'Secure': results_df['deadlocks'].str.startswith('Secure').sum(),
-            'Vulnerable': results_df['deadlocks'].str.startswith('Vulnerable').sum(),
-            'Inconclusive': results_df['deadlocks'].str.startswith('Inconclusive').sum()
+        CWE_1245_RESULTS_DF_COLS.DEADLOCKS.value: {
+            'Secure': CWE_1245_results_df[CWE_1245_RESULTS_DF_COLS.DEADLOCKS.value].str.startswith('Secure').sum(),
+            'Vulnerable': CWE_1245_results_df[CWE_1245_RESULTS_DF_COLS.DEADLOCKS.value].str.startswith('Vulnerable').sum(),
+            'Inconclusive': CWE_1245_results_df[CWE_1245_RESULTS_DF_COLS.DEADLOCKS.value].str.startswith('Inconclusive').sum()
         },
-        'LoC': results_df['LoC'].sum(),
-        'detection_time': results_df['detection_time'].sum()
     }])
-    results_df = pd.concat([results_df, total_row], ignore_index=True)
+    CWE_1245_results_df = pd.concat([CWE_1245_results_df, CWE_1245_total_row], ignore_index=True)
 
-    save_results(results_df)
+    save_results(CWE_1245_results_df, CWE_1233_results_df, detection_statistics_df)
 
 if __name__ == "__main__":
     main()
---
+++ CWE_Detection/enums.py
@@ -0,0 +1,19 @@
+from enum import Enum
+
+class CWE_1245_RESULTS_DF_COLS(Enum):
+    FILE_NAME = "File Name"
+    CASE_NUMBER = "Case Number"
+    STATE_COVERAGE = "State Coverage"
+    UNREACHABLE_STATES = "Unreachable States"
+    DEADLOCKS = "Deadlocks"
+
+class CWE_1233_RESULTS_DF_COLS(Enum):
+    FILE_NAME = "File Name"
+    SECURITY_SENSITIVE_REGISTERS = "Security Sensitive Registers"
+    LOCK_ENFORCEMENT = "Lock Enforcement"
+    SECURITY_SENSITIVE_REGISTER_COVERAGE = "Security Sensitive Register Coverage"
+
+class DETECTION_STATISTICS_DF_COLS(Enum):
+    FILE_NAME = "File Name"
+    LINES_OF_CODE = "LoC"
+    DETECTION_TIME = "Detection Time"
