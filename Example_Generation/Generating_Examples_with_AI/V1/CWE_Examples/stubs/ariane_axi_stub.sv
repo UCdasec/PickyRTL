b36d4ae740e983855dca3a4982ead7821e2db42a
John Parks <johnwparks03@gmail.com>
2026-02-24 18:12:51 -0500

    Revert "Added folder to store example generation"
    This reverts commit e6bfb4162dc3cdb9121dde2841b8a55b702cfbd3.

--- Example_Generation/Generating_Examples_with_AI/V1/CWE_Examples/stubs/ariane_axi_stub.sv
+++
@@ -1,4 +0,0 @@
-package ariane_axi;
-    typedef logic [0:0] req_t;
-    typedef logic [0:0] resp_t;
-endpackage
--- Example_Generation/Generating_Examples_with_AI/V1/CWE_Examples/stubs/sha256_k_constants_stub.sv
+++
@@ -1,6 +0,0 @@
-module sha256_k_constants (
-    input  logic [5:0]   addr,
-    output logic [31:0]  K
-);
-    // Stub: no logic
-endmodule
